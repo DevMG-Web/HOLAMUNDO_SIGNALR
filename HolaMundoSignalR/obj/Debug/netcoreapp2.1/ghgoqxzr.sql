b5fbaef85e77144667ae2f531b6888b588b47096
Miguel Gonzalez <mig_24@hotmail.com>
2021-09-25 23:46:00 -0500

    HolaMundo SignalR

---
+++ HolaMundoSignalR/obj/Debug/netcoreapp2.1/ghgoqxzr.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Personas] (
+    [Id] int NOT NULL IDENTITY,
+    [Nombre] nvarchar(max) NULL,
+    [Nombre2] nvarchar(max) NULL,
+    CONSTRAINT [PK_Personas] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20210220211026_Tabla Personas', N'2.1.14-servicing-32113');
+
+GO
+
